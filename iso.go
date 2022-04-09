8ac9f79362cecc51bc5b729fcebdb9f9e5e92e24
Wicken <ericghoubiguian@live.fr>
2022-04-09 18:23:16 +0200

    Correction of some errors...

--- iso.go
+++ iso.go
@@ -11,17 +11,17 @@ type ISO_date struct {
   //
   iso_year int
   iso_week_number int
-  iso_week_day Weekday
+  iso_week_day time.Weekday
 }
 
 //
-func (current_iso_date *ISO_date) InitializeWeather(dt time) {
+func (current_iso_date *ISO_date) InitializeWeather(dt time.Time) {
 	
     //
-    iso_date_s_year, iso_date_s_week := t.ISOWeek()
+    iso_date_s_year, iso_date_s_week := dt.ISOWeek()
 
     //
     current_iso_date.iso_year = iso_date_s_year
     current_iso_date.iso_week_number = iso_date_s_week
-    current_iso_date.iso_week_day = t.Weekday()
+    current_iso_date.iso_week_day = dt.Weekday()
 }
