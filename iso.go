ff4fbb7bafa95d0ab1c9bfdb8af0d02b90708f35
Wicken <ericghoubiguian@live.fr>
2022-04-09 15:05:55 +0200

    Update iso.go

--- iso.go
+++ iso.go
@@ -1,6 +1,20 @@
 package calendarConversionsGo
 
 //
+
+
+//
+const (
+        sunday = 0
+        monday = 1
+        tuesday = 2
+        wednesday = 3
+        thursday = 4
+        friday = 5
+        saturday = 6
+)
+
+//
 type iso_date struct {
   
   //
