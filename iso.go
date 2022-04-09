b3f07346d219d23d7d15c83ab303c94dc5c3da93
Wicken <ericghoubiguian@live.fr>
2022-04-09 18:08:28 +0200

    'int' to 'Weekday' for the 'iso_week_day' field...

--- iso.go
+++ iso.go
@@ -6,23 +6,12 @@ import (
 )
 
 //
-const (
-        sunday = 0
-        monday = 1
-        tuesday = 2
-        wednesday = 3
-        thursday = 4
-        friday = 5
-        saturday = 6
-)
-
-//
 type ISO_date struct {
   
   //
   iso_year int
   iso_week_number int
-  iso_week_day int
+  iso_week_day Weekday
 }
 
 //
