189904b83f0d35884c704848bdfd3b4e80ffa536
Wicken <ericghoubiguian@live.fr>
2022-04-10 11:20:19 +0200

    Definition of getters...

--- iso.go
+++ iso.go
@@ -25,3 +25,21 @@ func (current_iso_date *ISO_date) InitializeWeather(dt time.Time) {
     current_iso_date.iso_week_number = iso_date_s_week
     current_iso_date.iso_week_day = dt.Weekday()
 }
+
+//
+func (current_iso_date *ISO_date) Get_ISO_year() int {
+	
+	return current_iso_date.iso_year
+}
+
+//
+func (current_iso_date *ISO_date) Get_ISO_week_number() int {
+	
+	return current_iso_date.iso_week_number
+}
+
+//
+func (current_iso_date *ISO_date) Get_ISO_week_day() time.Weekday {
+	
+	return current_iso_date.iso_week_day
+}
