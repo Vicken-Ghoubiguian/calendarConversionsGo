a9986790f5c030cdccf22a538373dd3fdee38362
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-04-17 10:46:06 +0200

    Months in 'Positivists'

--- positivist.go
+++ positivist.go
@@ -2,6 +2,26 @@ package calendarConversionsGo
 
 import "fmt"
 
+//
+type Month int
+
+//
+const (
+	Moses Month = iota
+	Homer
+	Aristotle
+	Archimedes
+	Caesar
+	Saint_Paul
+	Charlemagne
+	Dante
+	Gutenberg
+	Shakespeare
+	Descartes
+	Frederic
+	Bichat
+)
+
 func main() {
 
 	fmt.Println("hello world")
