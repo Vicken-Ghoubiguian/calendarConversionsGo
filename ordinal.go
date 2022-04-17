866f4743cbc09169925c58c1c2c0c853f16dbfbd
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-04-17 10:10:15 +0200

    Addzersdzer

--- ordinal.go
+++ ordinal.go
@@ -1,6 +1,10 @@
 package calendarConversionsGo
 
-import "fmt"
+//
+import (
+	"fmt"
+	"time"
+)
 
 //
 type Ordinal_date struct {
@@ -8,6 +12,11 @@ type Ordinal_date struct {
 	//
 	year            int
 	day_of_the_year int
+}
+
+//
+func (current_ordinal_date *Ordinal_date) Initialize_ISO_week_based_calendar_date(dt time.Time) {
+
 }
 
 //
