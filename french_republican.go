f03c676b0b5222d5063ec86599f64505d0f1dc65
Wicken <ericghoubiguian@live.fr>
2022-06-18 09:30:41 +0200

    Update french_republican.go

--- french_republican.go
+++ french_republican.go
@@ -1,1 +1,20 @@
 package calendarConversionsGo
+
+type Month int
+
+ //
+ const (
+ 	Vendémiaire Month = iota
+ 	Brumaire
+ 	Frimaire
+ 	Nivôse
+ 	Pluviôse
+ 	Ventôse
+ 	Germinal
+ 	Floréal
+ 	Prairial
+ 	Messidor
+ 	Thermidor
+ 	Fructidor
+ 	Sans_culottides
+ )
