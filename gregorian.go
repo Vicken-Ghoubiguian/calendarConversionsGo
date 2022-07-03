ec838d947d507b2a9180e7803a9255759d07f00e
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-07-03 18:59:15 +0200

    JKL

--- gregorian.go
+++ gregorian.go
@@ -1,2 +1,19 @@
 package calendarConversionsGo
 
+type GregorianMonth int
+
+ //
+ const (
+	January GregorianMonth = iota
+	February
+	March
+	April
+	May
+	June
+	July
+	August
+	September
+	October
+	November
+	December
+ )
