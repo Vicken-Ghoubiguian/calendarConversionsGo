9aae1b2159900d04851ac984933c1892f1ca65f9
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-07-03 19:15:03 +0200

    JKKJ

--- julian.go
+++ julian.go
@@ -3,6 +3,41 @@ package calendarConversionsGo
 //
 /*import (
     "time"
-    "fmt"
-)*/
+)
 
+type julianMonth int
+
+ //
+ const (
+	January julianMonth = iota
+	February
+	March
+	April
+	May
+	June
+	July
+	August
+	September
+	October
+	November
+	December
+ )
+
+ //
+type Julian struct {
+
+	year int64
+	month julianMonth
+	day int64
+}
+
+//
+func (current_julian *Gregorian) Initialize_julian(dt time.Time) {
+
+}
+
+ //
+func (current_julian *Julian) Format() string {
+
+	return ""
+}*/
