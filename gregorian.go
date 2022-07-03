19f8d6dcb1004e4bed7e83cd49dc2b6a47397cc7
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-07-03 19:26:44 +0200

    KL

--- gregorian.go
+++ gregorian.go
@@ -18,6 +18,11 @@ func (current_gregorian *Gregorian) Initialize_gregorian(dt time.Time) {
 
 }
 
+//
+func (current_gregorian *Gregorian) Julian_to_gregorian(julianDate Julian) {
+
+}
+
  //
 func (current_gregorian *Gregorian) Format() string {
 
