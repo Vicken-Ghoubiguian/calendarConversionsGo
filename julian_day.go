c5314721e6c2b116cf59c6b4d4073556e8d9be44
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-07-03 11:28:47 +0200

    OPIO

--- julian_day.go
+++ julian_day.go
@@ -20,6 +20,14 @@ func (current_julian_day *JulianDay) Initialize_julian_day(dt time.Time) {
 }
 
 //
+func (current_julian_day *JulianDay) Determine_gregorian_date() {
+
+	/*year := dt.Year()
+	month := dt.Month()
+	day := dt.Day()*/
+}
+
+//
 func (current_julian_day *JulianDay) Format() string {
 
 	return ""
