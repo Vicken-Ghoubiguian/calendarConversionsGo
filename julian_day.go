ed4d17ababd75f1fec56333e5aa96399bab32467
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-07-03 11:28:04 +0200

    HIHOO

--- julian_day.go
+++ julian_day.go
@@ -20,12 +20,6 @@ func (current_julian_day *JulianDay) Initialize_julian_day(dt time.Time) {
 }
 
 //
-func (current_julian_day *JulianDay) Get_count_of_days_since_julian_period(dt time.Time) int64 {
-
-	return current_julian_day.countOfDaysSinceJulianPeriod
-}
-
-//
 func (current_julian_day *JulianDay) Format() string {
 
 	return ""
