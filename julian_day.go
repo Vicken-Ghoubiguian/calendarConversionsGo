dcc92461551c88083351a6969a29cb544ce52427
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-07-10 12:06:13 +0200

    For comments

--- julian_day.go
+++ julian_day.go
@@ -16,32 +16,38 @@ type JulianDay struct {
 //
 func (current_julian_day *JulianDay) Initialize_julian_day_from_time(dt time.Time) {
 
+	//
 	Y := float64(dt.Year())
 	M := float64(dt.Month())
 	D := float64(dt.Day())
 
+	//
 	A := math.Floor(Y/100)
 	B := math.Floor(A/4)
 	C := math.Floor(2 - A + B)
 	E := math.Floor(365.25 * (Y + 4716))
 	F := math.Floor(30.6001 * (M + 1))
 
+	//
 	current_julian_day.countOfDaysSinceJulianPeriod = C + D + E + F - 1524.5
 }
 
 //
 func (current_julian_day *JulianDay) Initialize_julian_day_from_gregorian_date(gregorian_date *Gregorian) {
 
+	//
 	Y := float64(gregorian_date.Get_year())
 	M := float64(gregorian_date.Get_monthNumber())
 	D := float64(gregorian_date.Get_day())
 
+	//
 	A := math.Floor(Y/100)
 	B := math.Floor(A/4)
 	C := math.Floor(2 - A + B)
 	E := math.Floor(365.25 * (Y + 4716))
 	F := math.Floor(30.6001 * (M + 1))
 
+	//
 	current_julian_day.countOfDaysSinceJulianPeriod = C + D + E + F - 1524.5
 }
 
