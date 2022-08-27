da3e1e44ca6bf21fbbaff42ad27679f62a19edb9
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-08-27 10:19:17 +0200

    ?KLLM

--- tests/julian_day_test.go
+++ tests/julian_day_test.go
@@ -1,9 +1,18 @@
 package main
 
-//import "github.com/Vicken-Ghoubiguian/calendar_conversions"
+import (
+	"time"
+	"github.com/Vicken-Ghoubiguian/calendar_conversions"
+)
 
 func main() {
 
 	//
-	//var test calendar_conversions.JulianDay
+	var test calendar_conversions.JulianDay
+
+	//
+	test.Initialize_julian_day_from_time(time.Now())
+
+	//
+	test.Format()
 }
