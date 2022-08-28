f604cea11f8a28174ae169624b0c47e93a2c88c6
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-08-28 18:33:36 +0200

    RttYtTRRRF

--- tests/test_julian_calendar/test_julian_calendar.go
+++ tests/test_julian_calendar/test_julian_calendar.go
@@ -8,5 +8,12 @@ import (
 
 func main() {
 
+	//
+	var current_julian_date calendar_conversions.Julian
 
+	//
+	current_julian_date.Initialize_julian(time.Now())
+
+	//
+	fmt.Println(current_julian_date.Format())
 }
