a04e1a22d0098253b9fcd0c2c2a8be816a583837
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-09-03 09:09:49 +0200

    JJJOO

--- julian.go
+++ julian.go
@@ -70,7 +70,53 @@ func (current_julian *Julian) Initialize_julian_from_time_golang(dt time.Time) {
 //
 func (current_julian *Julian) Initialize_julian_from_Gregorian(gregorianDate Gregorian) {
 
+	//
+	/*var jd JulianDay
 
+	//
+	jd.Initialize_julian_day_from_time(dt)
+
+	//
+	jd.Add_value_to_count_of_days_since_julian_period(0.5)
+	a := math.Floor(jd.Get_count_of_days_since_julian_period())
+	b := a + 1524
+	c := math.Floor((b - 122.1) / 365.25)
+	d := math.Floor(365.25 * c)
+	e := math.Floor((b - d) / 30.6001)
+	
+	//
+	month := -1
+
+	//
+	if e < 14 {
+
+		month = int(math.Floor(e - 1))
+
+	} else {
+
+		month = int(math.Floor(e - 13))
+	}
+
+	//
+	year := -1
+
+	//
+	if month > 2 {
+
+		year = int(math.Floor(c - 4716))
+
+	} else {
+
+		year = int(math.Floor(c - 4715))
+	}
+
+	//
+	day := b - d - math.Floor(30.6001 * e)
+	
+	//
+	current_julian.day = int64(day)
+	current_julian.month = time.Month(month)
+	current_julian.year = int64(year)*/
 }
 
 //
