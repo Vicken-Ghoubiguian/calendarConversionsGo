c25b6e63f15f230ee05a84c65e7c05b77e7d1fb8
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-09-03 08:44:42 +0200

    JKKKL

--- tests/tests_gregorian_dates_to_julian_dates/tests_gregorian_dates_to_julian_dates.go
+++ tests/tests_gregorian_dates_to_julian_dates/tests_gregorian_dates_to_julian_dates.go
@@ -9,8 +9,10 @@ import (
 func main() {
 
 	fmt.Println("===============================")
-	var noverber_7_1917_in_julian calendar_conversions.Julian
-	//noverber_7_1917_in_gregorian
-	noverber_7_1917_in_time_golang := time.Date(1917, time.Month(11), 7, 0, 0, 0, 0, time.UTC)
+	var november_7_1917_in_julian calendar_conversions.Julian
+	var november_7_1917_in_gregorian calendar_conversions.Gregorian
+	november_7_1917_in_time_golang := time.Date(1917, time.Month(11), 7, 0, 0, 0, 0, time.UTC)
+
+
 	fmt.Println("===============================")
 }
