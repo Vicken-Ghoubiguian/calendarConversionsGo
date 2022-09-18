5c7d47183ece034dcb425e92762d7aad6d02b446
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-09-18 09:52:11 +0200

    New testy...

---
+++ tests/test_julian_to_julian_day/test_julian_to_julian_day.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"time"
+	"fmt"
+	"github.com/Vicken-Ghoubiguian/calendar_conversions"
+)
+
+func main() {
+
+	//
+	var current_julian_day calendar_conversions.JulianDay
+
+	//
+	var current_julian_date calendar_conversions.Julian
+
+	//
+	current_julian_date.Initialize_julian_from_time_golang(time.Now())
+
+	//
+	current_julian_day.Initialize_julian_day_from_julian_date(current_julian_date)
+
+	//
+	fmt.Println(current_julian_day.Format())
+}
