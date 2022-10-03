d4ec517d8eeb5aa75ade889e18b428b8059c8ef6
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-10-03 15:03:33 +0200

    KLKLLK

--- gregorian.go
+++ gregorian.go
@@ -46,6 +46,7 @@ func (current_gregorian *Gregorian) Initialize_gregorian_from_time(dt time.Time)
 	current_gregorian.second = dt.Second()
 	//current_gregorian.microseconds = int64(dt.Microsecond())
 
+	fmt.Println("\nTest of Pr : " + string(dt.Hour()) + "\n")
 	fmt.Println("\nTest of Pr : " + string(current_gregorian.hour) + "\n")
 }
 
