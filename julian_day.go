ed91312be75a25ee168dea4ad9f74af2b6116c82
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-10-10 14:58:01 +0200

    KKLKLK

--- julian_day.go
+++ julian_day.go
@@ -54,8 +54,7 @@ func (current_julian_day *JulianDay) Initialize_julian_day_from_julian_date(juli
 	H := float64(julian_date.Get_hour()) / float64(24.0)
 	MIN := roundFloat((float64(julian_date.Get_minute()) / float64(24.0 * 60)), 5)
 	SEC := roundFloat((float64(julian_date.Get_second()) / (24.0 * 60 * 60)), 5)
-	//MILLISEC := dt.Microseconds() / (24.0 * 60 * 60 * 1000)
-	MILLISEC := float64(0)
+	MILLISEC := float64(julian_date.Get_microseconds() / (24.0 * 60 * 60 * 1000))
 
 	//
 	current_julian_day.countOfDaysSinceJulianPeriod = (E + F + D - 1524.5) + H + MIN + SEC + MILLISEC
